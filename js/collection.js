8b1f2bac61a27a28e09bab95d308efa8746e6e59
JakeKo <wijakoperski@gmail.com>
2017-11-03 21:11:33 -0500

    Added basic row and column manipulation methods to Matrix class

--- js/collection.js
+++ js/collection.js
@@ -1,19 +1,67 @@
-class Map {
-	
-}
-
 class Matrix {
 	constructor() {
-		this.grid = new Array();	
+		this.rows = new Array();	
 	}
 
-	addRow(index) {
-		let row = new Array();
+	pushRow(newRow) {
+		if (this.rows.length > 0)
+			console.assert(this.rows[0].length === newRow.length, 'New row is not of the same length as existing matrix');
+
+		this.rows.push(newRow);
 	}
 
-	addColumn(index) { }
+	insertRow(index, newRow) {
+		if (this.rows.length > 0)
+			console.assert(this.rows[0].length === newRow.length, 'New row is not of the same length as existing matrix');
 
-	removeRow(index) { }
+		this.rows.splice(index, 0, newRow);
+	}
 
-	removeColumn(index) { }
+	pushColumn(newColumn) {
+		console.assert(this.rows.length === newColumn.length, 'New column is not of the same length as existing matrix');
+
+		for (let i = 0; i < this.rows.length; i++)
+			this.rows[i].push(newColumn[i]);
+	}
+
+	insertColumn(index, newColumn) {
+		console.assert(this.rows.length === newColumn.length, 'New column is not of the same length as existing matrix');
+		
+		for (let i = 0; i < this.rows.length; i++)
+			this.rows[i].splice(index, 0, newColumn[i]);
+	}
+
+	popRow() {
+		return this.rows.pop();
+	}
+
+	removeRow(index) {
+		return this.rows.splice(index, 1);
+	}
+
+	popColumn() {
+		let column = new Array();
+
+		for (let i = 0; i < this.rows.length; i++)
+			column.push(this.rows[i].pop());
+
+		return column;
+	}
+
+	removeColumn(index) {
+		let column = new Array();
+
+		for (let i = 0; i < this.rows.length; i++)
+			column.push(this.rows[i].splice(index, 1)[0]);
+
+		return column;
+	}
+
+	isSquare() {
+		return this.isEmpty() ? true : this.rows.length === this.rows[0].length;
+	}
+
+	isEmpty() {
+		return this.rows.length === 0;
+	}
 }
