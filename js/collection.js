afb0f87440ad43c28c20a14f2f2683997e099a3e
JakeKo <wijakoperski@gmail.com>
2017-11-06 22:17:51 -0600

    Added some documentation

--- js/collection.js
+++ js/collection.js
@@ -3,10 +3,12 @@ module.exports.Matrix = class Matrix {
 		this.rows = new Array();	
 	}
 
+	// Returns an array of arrays representing the rows in the matrix
 	getRows() {
 		return this.rows;
 	}
 
+	// Returns an array of arrays representing the columns in the matrix
 	getColumns() {
 		let columns = new Array();
 
@@ -20,14 +22,17 @@ module.exports.Matrix = class Matrix {
 		return columns;
 	}
 
+	// Returns the length of the rows in the matrix
 	getWidth() {
 		return this.rows.length > 0 ? this.rows[0].length : 0;
 	}
 
+	// Returns the number of rows in the matrix
 	getHeight() {
 		this.rows.length;
 	}
 
+	// Returns a string of the matrix elements
 	toString() {
 		let s = '';
 
@@ -41,23 +46,28 @@ module.exports.Matrix = class Matrix {
 		return s;
 	}
 
+	// Adds a new row to the end of the matrix
 	pushRow(newRow) {
-		if (this.rows.length > 0)
-			console.assert(this.rows[0].length === newRow.length, 'New row is not of the same length as existing matrix');
+		// The new row must be of the same length as the other rows in the matrix
+		if (!this.isEmpty() && this.rows[0].length !== newRow.length) return false;
 
 		this.rows.push(newRow);
+		return true;
 	}
 
+	// Adds a new row to the matrix at the specified index
 	insertRow(index, newRow) {
-		if (this.rows.length > 0)
-			console.assert(this.rows[0].length === newRow.length, 'New row is not of the same length as existing matrix');
+		// The new row must be of the same length as the other rows in the matrix
+		if (!this.isEmpty() && this.rows[0].length !== newRow.length) return false;
 
 		this.rows.splice(index, 0, newRow);
+		return true;
 	}
 
+	// Adds a new column to the end of the matrix
 	pushColumn(newColumn) {
-		if (this.rows.length > 0)
-			console.assert(this.rows.length === newColumn.length, 'New column is not of the same length as existing matrix');
+		// The new column must be of the same length as the matrix
+		if (!this.isEmpty() && this.rows.length !== newColumn.length) return false;
 
 		if (this.rows.length === 0)
 			for (let i = 0; i < newColumn.length; i++) {
@@ -67,12 +77,15 @@ module.exports.Matrix = class Matrix {
 		else
 			for (let i = 0; i < this.rows.length; i++)
 				this.rows[i].push(newColumn[i]);
+
+		return true;
 	}
 
+	// Adds a new column to the matrix at the specified index
 	insertColumn(index, newColumn) {
-		if (this.rows.length > 0)
-			console.assert(this.rows.length === newColumn.length, 'New column is not of the same length as existing matrix');
-		
+		// The new column must be of the same length as the matrix
+		if (!this.isEmpty() && this.rows.length !== newColumn.length) return false;
+
 		if (this.rows.length === 0)
 			for (let i = 0; i < newColumn.length; i++) {
 				this.rows.push(new Array());
@@ -81,16 +94,21 @@ module.exports.Matrix = class Matrix {
 		else
 			for (let i = 0; i < this.rows.length; i++)
 				this.rows[i].splice(index, 0, newColumn[i]);
+
+		return true;
 	}
 
+	// Removes and returns the last row in the matrix
 	popRow() {
 		return this.rows.pop();
 	}
 
+	// Removes and returns the row in the matrix at the specified index
 	removeRow(index) {
 		return this.rows.splice(index, 1);
 	}
 
+	// Removes and returns the last column in the matrix
 	popColumn() {
 		let column = new Array();
 
@@ -100,6 +118,7 @@ module.exports.Matrix = class Matrix {
 		return column;
 	}
 
+	// Removes and returns the column in the matrix at the specified index
 	removeColumn(index) {
 		let column = new Array();
 
@@ -109,11 +128,22 @@ module.exports.Matrix = class Matrix {
 		return column;
 	}
 
+	// Returns true if the height and width of the matrix are equal
 	isSquare() {
-		return this.isEmpty() ? true : this.rows.length === this.rows[0].length;
+		return this.isEmpty() ? true : this.getHeight() === this.getWidth();
 	}
 
+	// Returns true if there are no elements in the matrix
 	isEmpty() {
-		return this.rows.length === 0;
+		return this.getHeight() === 0 || this.getWidth() === 0;
+	}
+
+	// Returns true if the matrix's elements are completely described by the elements in the specified array
+	isEnumerated(enums) {
+		for (let row of this.rows)
+			for (let element of row)
+				if (!enums.includes(element)) return false;
+
+		return true;
 	}
 }
