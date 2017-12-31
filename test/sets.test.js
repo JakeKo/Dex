bd0f1e0c535a2f1b8f8488bde258216295a1ab52
Jacob Koperski <wijakoperski@gmail.com>
2017-12-31 14:40:03 -0600

    Improved bad tests caused by sets appearing identical to deepEqual

--- test/sets.test.js
+++ test/sets.test.js
@@ -3,35 +3,46 @@ var TapSpec = require('tap-spec');
 Test.createStream().pipe(TapSpec()).pipe(process.stdout);
 var Sets = require('../js/sets.js');
 
-Test('The Sets class performs a union on two sets', (assert) => {
+// All sets are identical in the eyes of deepEqual
+// This ensures element-wise identicality
+var toSortedArray = (set) => {
+	return Array.from(set).sort();
+}
+
+Test('The Sets class performs a union on two nonempty sets', (assert) => {
 	let s1 = new Set([0, 2, 4, 6, 8]);
 	let s2 = new Set([1, 3, 5, 7, 9]);
-	assert.deepEqual(Sets.union(s1, s2), new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+	let union = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+	assert.deepEqual(toSortedArray(Sets.union(s1, s2)), union);
 	assert.end();
 });
 
-Test('The Sets class performs an intersection on two sets', (assert) => {
+Test('The Sets class performs an intersection on two nonempty, intersecting sets', (assert) => {
 	let s1 = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 	let s2 = new Set([1, 3, 5, 7, 9]);
-	assert.deepEqual(Sets.union(s1, s2), new Set([1, 3, 5, 7, 9]));
-	
-	s1 = new Set([0, 2, 4, 6, 8]);
-	s2 = new Set([1, 3, 5, 7, 9]);
-	assert.deepEqual(Sets.union(s1, s2), new Set([]));
+	let intersection = [1, 3, 5, 7, 9];
+
+	assert.deepEqual(toSortedArray(Sets.intersection(s1, s2)), intersection);
 	assert.end();
 });
 
-Test('The Sets class performs an exclusion on two sets', (assert) => {
+Test('The Sets class performs an exclusion on two nonempty, intersecting sets', (assert) => {
 	let s1 = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 	let s2 = new Set([1, 3, 5, 7, 9]);
-	assert.deepEqual(Sets.union(s1, s2), new Set([0, 2, 4, 6, 8]));
+	let exclusion = [0, 2, 4, 6, 8];
+
+	assert.deepEqual(toSortedArray(Sets.exclusion(s1, s2)), exclusion);
 	assert.end();
 });
 
-Test('The Sets class performs a set difference on two sets', (assert) => {
+Test('The Sets class performs a set difference on two nonempty, intersecting sets', (assert) => {
 	let s1 = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 	let s2 = new Set([1, 3, 5, 7, 9]);
-	assert.deepEqual(Sets.union(s1, s2), new Set([0, 2, 4, 6, 8]));
-	assert.deepEqual(Sets.union(s2, s1), new Set([]));
+	let diff1 = [0, 2, 4, 6, 8];
+	let diff2 = [];
+
+	assert.deepEqual(toSortedArray(Sets.difference(s1, s2)), diff1);
+	assert.deepEqual(toSortedArray(Sets.difference(s2, s1)), diff2);
 	assert.end();
 });
