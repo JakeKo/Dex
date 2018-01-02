1a8ef3ae5625f7f49df1b0429a2cd23f24a84217
Jacob Koperski <wijakoperski@gmail.com>
2018-01-02 09:22:06 -0600

    Added queue implementation in C#

--- C#/Dex/Dex/Program.cs
+++ C#/Dex/Dex/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Dex {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			var queue = new Queue<int>();
+
+			queue.Enqueue(2);
+			queue.Enqueue(6);
+			queue.Enqueue(1);
+			queue.Enqueue(9);
+			Console.WriteLine(ToStrings(queue.Elements));
+
+			queue.Dequeue();
+			Console.WriteLine(ToStrings(queue.Elements));
+
+			queue.Delete(7, 5);
+			Console.WriteLine(ToStrings(queue.Elements));
+		}
+
+		public static string ToStrings(List<int> list) {
+			var s = new StringBuilder();
+
+			for (int i = 0; i < list.Count; i++)
+				s.Append(list[i] + " ");
+
+			return s.ToString();
 		}
 	}
 }
---
+++ C#/Dex/Dex/Queue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Dex {
+	public class Queue<T> {
+		public List<T> Elements { get; set; }
+
+		public Queue(List<T> list = null) {
+			if (list != null)
+				this.Elements = new List<T>(list);
+			else
+				this.Elements = new List<T>(0);
+		}
+
+		// Returns the next element in the queue without removing it
+		public T Peek() {
+			return this.Elements[0];
+		}
+
+		// Adds an element to the end of the queue
+		public void Enqueue(T value) {
+			this.Elements.Add(value);
+		}
+
+		// Returns the next element in the queue
+		public T Dequeue() {
+			var value = this.Elements[0];
+			this.Elements.RemoveAt(0);
+			return value;
+		}
+
+		// Deletes the specified value from the queue the specified number of times
+		public void Delete(T value, int count = 1) {
+			for (int i = 0; i < count; i++) {
+				int index = this.Elements.IndexOf(value);
+
+				if (index >= 0 && index < this.Elements.Count)
+					this.Elements.RemoveAt(index);
+			}
+		}
+	}
+}
